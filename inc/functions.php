56cc14273a64f242c008ecd9e670999df8ae0115
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-19 15:53:40 -0600

    Add: add_project() create prepared statement that inserts category and project records into projects table

--- inc/functions.php
+++ inc/functions.php
@@ -14,3 +14,23 @@ function get_project_list(){
     }
 
 }
+
+function add_project($title, $category){
+    include 'connection.php';
+    //Insert record into title and cateogry
+    //Value placeholders 
+    $sql = 'INSERT INTO projects(title, category)  VALUE(?, ?)';
+
+    try {
+        //Pass $sql insert into prepared statement
+        $results = $db->prepare($sql);
+        //Bind $title argument to value placeholder and define parameter
+        $results->bindValue(1,$title, PDO::PARAM_STRING);
+        //Bind $category argument to value placeholder and define parameter
+        $results->bindValue(2,$category, PDO::PARAM_STRING);
+        //Execute the query
+        $result->execute();
+    }catch (Exception $e){
+        echo "Error: " . $e->getMessage() . "<br /> ";
+    }
+}
