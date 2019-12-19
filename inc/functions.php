9fe028d18adf724074a70e98e23d0201842b5802
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-19 13:57:47 -0600

    Add: get_project_list() to functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,2 +1,16 @@
 <?php
 //application functions
+
+function get_project_list(){
+    //Include db connction
+    include 'connection.php';
+
+    //Select from the projects table and add exception handling
+    try {
+    return $db->query('SELECT project_id, title, category FROM projects ');
+    } catch (Exception $e){
+        echo "Error!: " . $e->getMessage() . "</br>";
+        return false;
+    }
+
+}
