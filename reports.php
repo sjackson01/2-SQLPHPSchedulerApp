bb2fae8ddf2097fd500453ee9ee72405bd5ce29b
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-30 21:06:47 -0600

    Add(reports.php): Reports filter drop down menu

--- reports.php
+++ reports.php
@@ -12,6 +12,20 @@ include 'inc/header.php';
     <div class="col col-70-md col-60-lg col-center">
         <div class="col-container">
             <h1 class='actions-header'>Reports</h1>
+            <!-- Add form to manage reports --> 
+            <form class='form-container form-report' action='reports.php' method='get'>
+                <label for='filter'>Filter: </label>
+                <select id='filter' name='filter'>
+                    <option value=''>Select One</option>
+                    <!-- Use PHP to pull project information with a foreach loop --> 
+                    <?php
+                        foreach(get_project_list() as $item){
+                            echo '<option value="project:' . $item['project_id'] . '">';
+                            echo $item['title'] . "</option>\n";
+                        }
+                    ?>
+                </select>
+                <input class="button" type="submit" value="Run" />
         </div>
         <div class="section page">
             <div class="wrapper">
